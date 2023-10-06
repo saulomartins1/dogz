2e32083233332464ab426187f75dba6912cc678f
d3Niro <saulomartins2020@gmail.com>
2023-10-06 12:11:26 -0300

    Added: Header.jsx - Footer.jsx - Home.jsx | CSS Modules | Router-DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 //Global styles
 import './App.css'
 
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Home from './Components/Home'
+
 
 function App() {
-    console.log("Hello, world!")
     return (
-        <>
-        </>
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
     )
 }
 
