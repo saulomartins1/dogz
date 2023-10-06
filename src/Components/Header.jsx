cec3022d9f76210307e6c80102219a2274174c0c
d3Niro <saulomartins2020@gmail.com>
2023-10-06 13:55:46 -0300

    Added: Assets | vite-svg-plugin updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import DogzLogo from '../Assets/dogz.svg?react'
 
 function Header() {
     return (
         <header className={styles.header}>
             <nav>
-                <Link to='/'>Home</Link>
+                <Link to='/' aria-label='Dogz - Home'>
+                    <DogzLogo />
+                </Link>
                 <Link to='/login'>Entrar / Cadastrar</Link>
             </nav>
         </header>
