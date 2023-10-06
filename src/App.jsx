364637fd1a671f3275864be9c53f5364fa75b235
d3Niro <saulomartins2020@gmail.com>
2023-10-06 11:57:03 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//Global styles
+import './App.css'
+
+
+function App() {
+    console.log("Hello, world!")
+    return (
+        <>
+        </>
+    )
+}
+
+export default App
