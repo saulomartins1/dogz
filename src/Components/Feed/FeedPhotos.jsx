aa4b8771586b84a8007777ba506df11bafd601d8
d3Niro <saulomartins2020@gmail.com>
2023-10-19 18:17:19 -0300

    feat: infinite scroll in <Feed /> photos (with comments)

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,17 +6,26 @@ import { PHOTOS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css'
 
-function FeedPhotos({ user, setModalPhoto }) {
+function FeedPhotos({ user, page, setInfinite, setModalPhoto }) {
     const { data, error, loading, request } = useFetch();
 
     React.useEffect(() => {
-        const fetchPhotos = async () => {
-            const { url, options } = PHOTOS_GET({ page: 1, total: 3, user });
+        let total = 3; // quantidade de páginas que vão aparecer no feed (recebidas no fetch)
 
+        const fetchPhotos = async () => {
+            // #2 (3/3) - page: faz o fecth da página atual;
+            const { url, options } = PHOTOS_GET({ page, total, user });
             const { response, json } = await request(url, options);
+
+
+            // #3 (3/3) - Teve resposta, status foi ok, mas o json veio como array vazia (sem os objetos de cada foto)...
+            // ...não é mais infinito (não ativa a lógica setar +1 página)
+            if (response && response.ok && json.length < total) {
+                return setInfinite(false)
+            }
         }
         fetchPhotos();
-    }, [request, user]);
+    }, [request, user, page, setInfinite]);
 
     if (error) return <Error error={error} />
     if (loading) return <Loading />
