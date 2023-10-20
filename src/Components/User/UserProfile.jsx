38e7c845f03eefe396858c6b17c582776aa4396b
d3Niro <saulomartins2020@gmail.com>
2023-10-20 10:49:30 -0300

    test

---
+++ src/Components/User/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Feed from '../Feed/Feed'
+
+function UserProfile() {
+    const { user } = useParams();
+
+    return (
+        <section>
+            <h1 className='title' style={{ margin: '1.9rem 0' }}>{user}</h1>
+            <Feed user={user} />
+        </section>
+    )
+}
+
+export default UserProfile
